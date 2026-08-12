fbc683f1759eda572c5c7066f997bb759960ea33
danieli-dutra <danieli.dutra1@gmail.com>
2026-08-12 19:51:14 -0300

    fix(frontend): correct logo import paths

--- frontend-v2/src/components/navigation/Footer/Footer.jsx
+++ frontend-v2/src/components/navigation/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import Logo from "@/components/branding/Logo";
+import Logo from "@/components/branding/logo";
 
 import { useTheme } from "@/contexts/ThemeContext";
 import { THEMES } from "@/utils/theme";
