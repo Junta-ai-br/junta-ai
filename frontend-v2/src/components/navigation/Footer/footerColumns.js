9b1b17ab4d3b95cd5dca1cd71f575e9b893dbed7
danieli-dutra <danieli.dutra1@gmail.com>
2026-08-18 22:22:57 -0300

    feat(frontend): configure footer page links

--- frontend-v2/src/components/navigation/Footer/footerColumns.js
+++ frontend-v2/src/components/navigation/Footer/footerColumns.js
@@ -4,21 +4,21 @@ const footerColumns = [
     links: [
       { label: "Recursos", href: "/#features" },
       { label: "Como funciona", href: "/#como-funciona" },
-      { label: "Planos", href: "/#planos" },
+      { label: "Planos", href: "/planos" },
       { label: "Atualizações", href: "/#atualizacoes" },
     ],
   },
   {
     title: "Company",
-    links: [{ label: "Sobre nós", href: "/#sobre-nos" }],
+    links: [{ label: "Sobre nós", href: "/sobre" }],
   },
   {
     title: "Suporte",
     links: [
       { label: "Central de ajuda", href: "/#duvidas" },
-      { label: "Contato", href: "/#contato" },
-      { label: "Privacidade", href: "/#privacidade" },
-      { label: "Termos de uso", href: "/#termos-de-uso" },
+      { label: "Contato", href: "/contato" },
+      { label: "Privacidade", href: "/privacidade" },
+      { label: "Termos de uso", href: "/termos" },
     ],
   },
 ];
